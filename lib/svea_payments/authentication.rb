2bd78e6ec63d286d13ece8e6bbfc340fe82756e5
Antti Akonniemi <antti@kiskolabs.com>
2025-05-18 09:46:03 +0300

    Document basic auth token

--- lib/svea_payments/authentication.rb
+++ lib/svea_payments/authentication.rb
@@ -4,11 +4,11 @@ require 'base64'
 module SveaPayments
   class Authentication
     extend Base
+    # Returns a value suitable for the HTTP Authorization header
+    # using basic access authentication.
     def self.get_basic_auth_token(username, password)
       base64_user_pass = Base64.strict_encode64("#{username}:#{password}")
-      uri = URI("#{SveaPayments.base_url}/NewPaymentExtended.pmt")
-      request = Net::HTTP::Get.new(uri)
-      request["Authorization"] = "Basic #{base64_user_pass}"
+      "Basic #{base64_user_pass}"
     end
   end
 end
